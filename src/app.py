045db7d2e3488f3546430d78d244ceefffd9f2ef
mdazg10 <mascaniog20@gmail.com>
2022-10-21 20:31:05 -0500

    partido

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import config
 import routes.mesa as rm
+import routes.partidos as rp
 from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
 app.config['MONGODB_HOST'] = config['MONGO_URI']
@@ -20,6 +22,16 @@ app.add_url_rule('/regisNal/<n_mesa>', 'find_table', rm.find_table)
 app.add_url_rule('/regisNal', 'insert_table', rm.insert_table, methods= ['POST'])
 app.add_url_rule('/regisNal/<n_mesa>', 'update_table', rm.update_table, methods= ['PUT'])
 app.add_url_rule('/regisNal/<n_mesa>', 'delete_table', rm.delete_table, methods= ['DELETE'])
+
+
+#partidos
+app.add_url_rule('/regisNal', 'find_partidos', rp.find_partidos)
+app.add_url_rule('/regisNal/<nombre_partido>', 'find_partido', rp.find_partido)
+app.add_url_rule('/regisNal', 'insert_partido', rp.insert_partido, methods= ['POST'])
+app.add_url_rule('/regisNal/<nombre_partido>', 'update_partido', rp.update_partido, methods= ['PUT'])
+app.add_url_rule('/regisNal/<nombre_partido>', 'delete_partido', rp.delete_partido, methods= ['DELETE'])
+
+
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
---
+++ src/routes/partidos.py
@@ -0,0 +1,88 @@
+from flask import jsonify, request, make_response
+from controllers.partidos import PartidoController
+
+partido_controller = PartidoController()
+
+# create partido
+
+def insert_partido():
+    body = request.get_json()
+    try:
+        partidos = partido_controller.get_by_id(body['nombre_partido'])
+        if partidos:
+            return make_response({
+                'message': 'El partido' + body['nombre_partido'] + ' Ya está registrado en el sistema'
+            }, 400)
+        partido_controller.create(body)
+        return make_response({
+            'message': 'El partido' + body['nombre_partido'] + ' ha sido creado satisfactoriamente.'
+        }, 201)
+    except Exception as ex:
+        print(ex)
+        return make_response({
+            'message': 'Hubo un error en la creación del partido'
+        }, 500)
+
+# Find partidos
+def find_partidos():
+    try:
+        partidos = partido_controller.get_all()
+        return make_response(jsonify(partidos), 200)
+    except Exception as ex:
+        print(ex)
+        return make_response({
+            'message': 'Hubo un error al obtener la información de los partidos'
+        }, 500)
+
+# Find partido
+def find_partido(nombre_partido):
+    try:
+        partidos = partido_controller.get_by_id(nombre_partido)
+        if partidos:
+            return make_response(jsonify(partidos), 200)
+        else:
+            return make_response({
+                'message': 'El partido' + nombre_partido + ' no fue encontrado'
+            }, 404)
+    except Exception as ex:
+        print(ex)
+        return make_response({
+            'message': 'Hubo un error al obtener la información del partido'
+        }, 500)
+
+# delete partido
+def delete_partido(nombre_partido):
+    try:
+        delete = partido_controller.delete(nombre_partido)
+        if delete:
+            return make_response({
+                'message': 'El partido' + nombre_partido + ' fue eliminado satisfactoriamente'
+            }, 200)
+        else:
+            return make_response({
+                'message': 'El partido' + nombre_partido + ' no fue encontrado'
+            }, 404)
+    except Exception as ex:
+        print(ex)
+        return make_response({
+            'message': 'Hubo un error al eliminar el partido'
+        }, 500)
+
+# update partido
+def update_partido(nombre_partido):
+    body = request.get_json()
+    try:
+        update = partido_controller.update(nombre_partido, body)
+        if update:
+            return make_response({
+                'message': 'El partido' + nombre_partido + ' fue actualizado satisfactoriamente'
+            }, 200)
+        else:
+            return make_response({
+            'message': 'No hay partido con el nombre ' + nombre_partido
+        }, 400)
+    except Exception as ex:
+        print(ex)
+        return make_response({
+            'message': 'Hubo un error al actualizar la información del partido'
+        }, 500)
