796c0271538f6dd9151150bcddf992287e303ef1
Diego Fernando Campos Alba <111471379+dfcamposalba@users.noreply.github.com>
2022-10-22 18:26:19 -0500

    Candidatos

--- src/app.py
+++ src/app.py
@@ -21,6 +21,14 @@ app.add_url_rule('/regisNal', 'insert_table', rm.insert_table, methods= ['POST']
 app.add_url_rule('/regisNal/<n_mesa>', 'update_table', rm.update_table, methods= ['PUT'])
 app.add_url_rule('/regisNal/<n_mesa>', 'delete_table', rm.delete_table, methods= ['DELETE'])
 
+#candidatos
+app.add_url_rule('/', 'index', index)
+app.add_url_rule('/regisNal', 'find_candidato', rm.find_candidato)
+app.add_url_rule('/regisNal/<nameCandidato>', 'find_candidato', rm.find_candidato)
+app.add_url_rule('/regisNal', 'insert_candidato', rm.insert_candidato, methods= ['POST'])
+app.add_url_rule('/regisNal/<n_mesa>', 'update_candidato', rm.update_candidato, methods= ['PUT'])
+app.add_url_rule('/regisNal/<n_mesa>', 'delete_candidato', rm.delete_candidato, methods= ['DELETE'])
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.run()
